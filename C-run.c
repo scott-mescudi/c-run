145ddf39369e70918730f89b9b6abc1e32091d41
script kiddie <anish.chittu2007@gmail.com>
2024-10-01 15:58:26 +0200

    made file check universal

--- C-run.c
+++ C-run.c
@@ -5,13 +5,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#ifdef WIN32
-#include <io.h>
-#define F_OK 0
-#define access _access
-#else
-#include <unistd.h>
-#endif
+
 
 #define RESET   "\033[0m"
 #define GREEN   "\033[32m"
@@ -22,7 +16,6 @@ void build(const char* filename, const char* exeFilename, const char* ext) {
     
     if (command == NULL) {
         printf("Memory allocation failed!\n");
-        free(command);
     }
 
     snprintf(command, commandSize, "gcc %s -o %s%s", filename, exeFilename, ext);
@@ -57,14 +50,13 @@ char* getExeName(const char* filename) {
     return fname;
 }
 
-
-
 int exists(const char *fname) {
-    if (access(fname, F_OK) == 0) {
-        return 1;
-    } else {
-        return 0; 
+    FILE *file = fopen(fname, "r");
+    if (file) {
+        fclose(file);
+        return 0;
     }
+    return 1;
 }
 
 void run(const char* exeFilename, const char* ext){
@@ -75,8 +67,9 @@ void run(const char* exeFilename, const char* ext){
         printf("Memory allocation failed!\n");
         return;
     }
+
     int i = exists(exeFilename);
-    if (i == 1) {
+    if (i == -1) {
         printf("Executable not found.\n");
         return;
     }
@@ -117,6 +110,12 @@ bool is_valid_filename(const char* filename) {
 }
 
 int main(int argc, char* argv[]) {
+    #ifdef _WIN32
+        char* ext = ".exe";
+    #else
+        char* ext = "";
+    #endif
+
     if (argc > 1){
         if (strcmp(argv[1], "build") == 0){
             if (argc > 2){
@@ -124,7 +123,10 @@ int main(int argc, char* argv[]) {
                 
                 if (result) {
                     char* exeName = getExeName(argv[2]);
-                    char* ext = ".exe";
+                    if (exeName == NULL){
+                        printf("Failed to get exename.");
+                        return 1;
+                    }
                     build(argv[2], exeName, ext);
                     if (exeName != NULL){
                         free(exeName);
@@ -139,7 +141,11 @@ int main(int argc, char* argv[]) {
                 bool result = is_valid_filename(argv[2]);
                 if (result) {
                     char* exeName = getExeName(argv[2]);
-                    char* ext = ".exe";
+                    if (exeName == NULL){
+                        printf("Failed to get exename.");
+                        return 1;
+                    }
+
                     build(argv[2], exeName, ext);
                     run(exeName, ext);
                     if (exeName != NULL){

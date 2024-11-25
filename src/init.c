114f5391ba015264f147c1ef7079cce275960505
script kiddie <anish.chittu2007@gmail.com>
2024-11-25 20:19:56 +0100

    linux support

--- src/init.c
+++ src/init.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <direct.h>
+
 #include <sys/stat.h>
+
+#ifdef _WIN32
+    #include <direct.h>
+    #define MKDIR(dir) _mkdir(dir) 
+#else
+    #define MKDIR(dir) mkdir(dir, 0755) 
+#endif
+
+
 
 const char DATA[256] = "#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char* argv[]){\n\tprintf(\"Hello, World!\");\n\n\treturn 0;\n}";
 
@@ -12,7 +21,7 @@ void init(char* projectName){
         return;
     }
 
-    if (mkdir(projectName) == 0) { 
+    if (MKDIR(projectName) == 0) { 
         printf("[+] Successfully initialize '%s'.\n", projectName);
     } else {
         printf("[-] Error creating directory '%s'", projectName);
@@ -24,7 +33,7 @@ void init(char* projectName){
         char subDirPath[256];
         snprintf(subDirPath, sizeof(subDirPath), "%s/%s", projectName, subdirs[i]);
 
-        if (mkdir(subDirPath) == 0) { 
+        if (MKDIR(subDirPath) == 0) { 
             printf("[+] Successfully created subdirectory '%s' inside project '%s'.\n", subDirPath, projectName);
         } else {
             printf("[-] Error creating subdirectory '%s'.\n", subDirPath);
